732d8f9f315746eb1f0699fbed16f9f39193301f
Tim <ccu.schaller@outlook.com>
2023-01-11 12:05:41 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-const splitWords = id  => { // Yes I know could be done on the server side, but im to lazy to write it all down .-.
-    const elements = document.getElementsByClassName(id)
-    for (let i = 0; i < elements.length; i++) {
-        const element = elements[i]
-        const text = element.innerText
-        const words = text.split('')
-        element.innerHTML = words.map(word => `<span class="letter">${word}</span>`).join(' ')
-    }
-
-}
 let bonk = false
 function bonkTimer() {
     if (bonk) {
