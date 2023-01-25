fb6bed4f9867fffdf149bc6a1858aed0d6a2378c
Tim <59140326+tim2zg@users.noreply.github.com>
2023-01-25 16:37:59 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let bonk = false
+let boxes = false
 function bonkTimer() {
     if (bonk) {
         let audio = new Audio('/assets/Bonkt.mp3')
@@ -6,3 +7,23 @@ function bonkTimer() {
     }
 }
 
+function toggleCollection() {
+    boxes = !boxes
+    if (boxes) {
+        const title = document.getElementById("title")
+        title.style.outline = "wheat 3px double"
+        title.style.outlineOffset = "5px"
+        const boxes = document.getElementsByClassName("boxes")
+        boxes[0].style.display = "grid"
+        boxes[1].style.display = "grid"
+        boxes[2].style.display = "grid"
+    } else {
+        const title = document.getElementById("title")
+        title.style.outline = "none"
+        title.style.outlineOffset = "none"
+        const boxes = document.getElementsByClassName("boxes")
+        boxes[0].style.display = "none"
+        boxes[1].style.display = "none"
+        boxes[2].style.display = "none"
+    }
+}
