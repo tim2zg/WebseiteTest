62486cb65b96ff2db46c6977390956b3e76942ca
tim2zg <ccu.schaller@outlook.com>
2023-02-21 16:46:10 +0100

    first commit

---
+++ js/content.js
@@ -0,0 +1,33 @@
+// On document ready without jQuery
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Split the text into phrases
+    const phrases = document.getElementById("text").textContent.split(".")
+    // Split into spans
+    let counter = 0;
+    const spans = phrases.map(phrase => {
+        counter++
+        const span = document.createElement("span")
+        span.innerText = phrase
+        span.id = counter.toString()
+        return span
+    });
+
+    // Add hover event to the spans
+    spans.forEach(span => {
+        span.addEventListener("mouseover", function() {
+            console.log("Hovered" + span)
+        })
+        span.addEventListener("mouseout", function() {
+            span.classList.remove("hover")
+        })
+    });
+
+    // Clear the text
+    document.getElementById("text").textContent = ""
+    // Append the spans
+    spans.forEach(span => {
+        document.getElementById("text").appendChild(span)
+    });
+
+});
